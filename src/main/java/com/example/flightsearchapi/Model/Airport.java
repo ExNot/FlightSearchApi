4134b8e9b40d42a6fa91885db09a81df11a30302
Enes Başkale <enesbaskale@gmail.com>
2024-01-06 13:56:00 +0300

    done without custom validations, authentication, scheduled bg job

--- src/main/java/com/example/flightsearchapi/Model/Airport.java
+++ src/main/java/com/example/flightsearchapi/Model/Airport.java
@@ -4,13 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class Airport {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
 
+    @Size(min= 1, max = 45)
     private String city;
 
 
